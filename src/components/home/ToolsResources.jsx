4bf6f03441841679fc480e4345bb3e2d75857ea9
@benjamin lucky <bamidelebenjamin5@gmail.com>
2025-08-17 15:47:49 +0100

    Homepage Grid adjustment on tools and resources

--- src/components/home/ToolsResources.jsx
+++ src/components/home/ToolsResources.jsx
@@ -98,7 +98,7 @@ const ToolsResources = () => {
 
       {/* Tools Cards */}
       <motion.div
-        className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8 max-w-7xl mx-auto px-4"
+        className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-8 max-w-7xl mx-auto px-4"
         variants={sectionVariants}
       >
         {tools.map((tool) => (
